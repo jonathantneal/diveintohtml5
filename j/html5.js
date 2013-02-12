7e9698772062fa5926f4ed757f33f96ffa3c1e14
Jonathan Neal <jonathantneal@hotmail.com>
2013-02-12 14:16:17 -0800

    First commit for Book edition

--- j/html5.js
+++ j/html5.js
@@ -1,13 +1,15 @@
 // html5shiv (lite) | @afarkas @jdalton @jon_neal @rem | MIT/GPL2
 
 // shiv elements on the document
-"abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section subline summary time video".replace(/\w+/g, function (nodeName) {
+"abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup main mark meter nav output progress section subline summary time video".replace(/\w+/g, function (nodeName) {
 	document.createElement(nodeName);
 });
 
 // prepend html5 styles to the document
 (function (element) {
-	element.innerHTML = "x<style>article,aside,details,figcaption,figure,footer,header,hgroup,nav,section,subline,summary{display:block}audio,canvas,video{display:inline-block}audio:not([controls]){display:none;height:0}[hidden]{display:none}</style>";
+	element.innerHTML = "x<style>article,aside,details,figcaption,figure,footer,header,hgroup,main,nav,section,subline,summary{display:block}audio,canvas,video{display:inline-block}audio:not([controls]){display:none;height:0}[hidden]{display:none}</style>";
 
-	document.documentElement.insertBefore(element.lastChild, document.documentElement.firstChild);
+	var head = (document.getElementsByTagName("head")[0] || document.documentElement.getElementsByTagName("*")[0]);
+
+	head.insertBefore(element.lastChild, head.firstChild);
 })(document.createElement("x"));
